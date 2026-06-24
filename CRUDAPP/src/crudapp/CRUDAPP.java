99dbe97404c3360bf42b3c64ddc974431d56690f
vahidcg <122486028+vahidcg@users.noreply.github.com>
2026-06-24 19:29:27 +0200

    Add files via upload

---
+++ CRUDAPP/src/crudapp/CRUDAPP.java
@@ -0,0 +1,15 @@
+package crudAPP;
+
+
+import com.Login.LoginFrame; 
+public class CRUDAPP {
+
+    public static void main(String[] args) {
+       // create object of that login frame for show when you run this project login frame must be show first...
+        
+       LoginFrame loginObj = new LoginFrame();
+       loginObj.show();
+               
+    }
+    
+}
